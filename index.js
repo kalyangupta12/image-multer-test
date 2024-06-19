fb027e56fd0569ab5bed787fe84c9db602ef0114
Kalyan Gupta <90916009+kalyangupta12@users.noreply.github.com>
2024-06-19 10:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/upload', upload.single('profileImage'), (req, res) => {
     console.log(req.body);
     console.log(req.file);
 
-    return res.redirect('/');
+    return res.redirect('/home');
 });
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
